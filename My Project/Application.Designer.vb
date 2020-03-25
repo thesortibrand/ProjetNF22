b512e91e7f5aac00e10809b8162b6575beac3c82
utcpret <utcpret@DSIPRETETU39>
2020-03-25 18:19:29 +0100

    Finalisation Question 10

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ProjetNF22.Question8
+            Me.MainForm = Global.ProjetNF22.Question10
         End Sub
     End Class
 End Namespace
